5fc140ffd82d29420bbee55d04f4b16dfb80d0d7
Jaron Heard <jaronheard@gmail.com>
2020-06-15 01:03:25 -0400

    remove unused code

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import Wrapper from "../components/Wrapper";
 
 const NotFoundPage = () => (
   <Wrapper>
-    <div heroTitle="Civic and Gatsby Starter" heroSubtitle="They play nice!">
+    <div>
       <SEO title="404: Not found" />
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
