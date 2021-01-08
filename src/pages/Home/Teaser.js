50ee70d70153a32c689456023c7349312d0815c8
Dianna Faulk <diannafaulk@gmail.com>
2021-01-08 15:56:50 -0800

    Add contentful teaser copy

--- src/pages/Home/Teaser.js
+++ src/pages/Home/Teaser.js
@@ -1,40 +1,50 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
-import { Link } from "gatsby";
+import { useStaticQuery, graphql, Link } from "gatsby";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 import ContentContainer from "../../components/ContentContainer";
 import GridSingle from "../../components/GridSingle";
 
-const Teaser = () => (
-  <ContentContainer margin="sm">
-    <GridSingle
-      containerStyle={css`
-        padding: 0 20px;
-      `}
-    >
-      <p>
-        <b>Focus on Data Context:</b> Civic Software Foundation is focusing
-        throughout 2021 on creating new context systems that provide ways to
-        collect, curate, use, and share data that increase equity and justice.
-        Our programs bring together the voices of people represented by data,
-        the experience of community organizations, and institutional
-        partnerships to collaborate on creating an entirely new way of engaging
-        with data.
-      </p>
-      <Link
-        to="google.com"
-        className="btn-pink"
-        css={css`
-          margin-bottom: 20px;
+const Teaser = () => {
+  const { contentfulCallToActionSimple } = useStaticQuery(
+    graphql`
+      {
+        contentfulCallToActionSimple(
+          contentful_id: { eq: "4xAM1s3NoGu4eCpkLGBa27" }
+        ) {
+          buttonLink
+          buttonTitle
+          summary {
+            json
+          }
+        }
+      }
+    `
+  );
+
+  const { summary, buttonLink, buttonTitle } = contentfulCallToActionSimple;
+
+  return (
+    <ContentContainer margin="sm">
+      <GridSingle
+        containerStyle={css`
+          padding: 0 20px;
         `}
       >
-        <p>Learn about context</p>
-      </Link>
-      <Link to="google.com" className="btn-yellow">
-        <p>Get involved</p>
-      </Link>
-    </GridSingle>
-  </ContentContainer>
-);
+        {documentToReactComponents(summary.json)}
+        <Link
+          to={buttonLink}
+          className="btn-pink"
+          css={css`
+            margin-bottom: 20px;
+          `}
+        >
+          <p>{buttonTitle}</p>
+        </Link>
+      </GridSingle>
+    </ContentContainer>
+  );
+};
 
 export default Teaser;
